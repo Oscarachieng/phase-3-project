721aa23d4ce7927ec191906acdb5675c7f5c3d31
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-07-27 17:22:57 +0300

    Adds header stylings

--- src/Header.js
+++ src/Header.js
@@ -7,27 +7,31 @@ const Header = () => {
     <div className="header">
       <div className="upper-section">
         <h2 className="lydo">
-          {" "}
-          Smart <span> Lydo </span>
+          {/* {" "} */}
+          Smart<span>Lydo</span>
         </h2>
-        <SearchBar />
-        <img
-          src="../src/public/Assets/images/heart.png"
-          height="5px"
-          alt="like"
-        />
-        <img
-          src="../src/public/Assets/images/carts.png"
-          height="5px"
-          alt="shopping-cart"
-        />
-        <img
-          src="../src/public/Assets/images/13556223.jpg"
-          height="5px"
-          alt="may"
-        />
+        <div>
+          <SearchBar />
+        </div>
+        <div className="image-container">
+          <img
+            src="../src/public/Assets/images/heart.png"
+            height="5px"
+            alt="like"
+          />
+          <img
+            src="../src/public/Assets/images/carts.png"
+            height="5px"
+            alt="shopping-cart"
+          />
+          <img
+            src="../src/public/Assets/images/13556223.jpg"
+            height="5px"
+            alt="may"
+          />
+        </div>
       </div>{" "}
-      <div className="lower-section">
+      <div className="lower-section" style={{marginLeft:250}}>
         <NavBar />
       </div>{" "}
     </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,30 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "6px",
+    margin: "0 20px 6px",
+    background: "orange",
+    textDecoration: "none",
+    color: "Black",
+  };
+
 const NavBar = () => {
   return (
-    <nav className="navigation-button">
-      <NavLink to="/" exact>
+    <nav className="navigation-buttons">
+      <NavLink to="/" exact  style={linkStyles}>
         Home
       </NavLink>
 
-      <NavLink to="/about" exact>
+      <NavLink to="/about" exact style={linkStyles}>
         About
       </NavLink>
-      <NavLink to="/productlist" exact>
+      <NavLink to="/productlist" exact style={linkStyles}>
         ProductList
       </NavLink>
-      <NavLink to="/cart" exact>
+      <NavLink to="/cart" exact style={linkStyles}>
         Cart
       </NavLink>
-      <NavLink to="/reviews" exact>
+      <NavLink to="/reviews" exact style={linkStyles}>
         Reviews
       </NavLink>
-      <NavLink to="/sign-up" exact>
+      <NavLink to="/sign-up" exact style={linkStyles}>
         MyAccount
       </NavLink>
 
-      <NavLink to="/login" exact>
+      <NavLink to="/login" exact style={linkStyles}>
         Sign-n
       </NavLink>
     </nav>
