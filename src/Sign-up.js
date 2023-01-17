07c2efba22766b98a56be7ee55a720248ee3e163
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2023-01-17 14:51:07 +0300

    Update Sign-up.js

--- src/Sign-up.js
+++ src/Sign-up.js
@@ -1,149 +1,113 @@
 import { useRef, useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// ?import { Link, useNavigate } from "react-router-dom";
 
-const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+// const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+// const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Sign = () => {
-  const userRef = useRef();
-  const navigate = useNavigate();
+//   const userRef = useRef();
+//   const navigate = useNavigate();
 
-  const [name, setName] = useState("");
-  const [validName, setValidName] = useState(false);
-  const [userFocus, setUserFocus] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [course, setCourse] = useState("");
 
-  const [email, setEmail] = useState("");
+  const [year_study, setYear_study] = useState("");
 
-  const [password, setPassWord] = useState("");
-  const [validPassWord, setValidPassWordd] = useState(false);
-  const [passWordFocus, setPassWordFocus] = useState(false);
+//   const [password, setPassWord] = useState("");
+//   const [validPassWord, setValidPassWordd] = useState(false);
+//   const [passWordFocus, setPassWordFocus] = useState(false);
 
-  const [location, setLocation] = useState("");
+//   const [location, setLocation] = useState(2000);
 
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
+//   useEffect(() => {
+//     userRef.current.focus();
+//   }, []);
 
-  useEffect(() => {
-    setValidName(USER_REGEX.test(name));
-  }, [name]);
+//   useEffect(() => {
+//     setValidName(USER_REGEX.test(name));
+//   }, [name]);
 
-  useEffect(() => {
-    setValidPassWordd(PWD_REGEX.test(password));
-  }, [password]);
+//   useEffect(() => {
+//     setValidPassWordd(PWD_REGEX.test(password));
+//   }, [password]);
 
   const handleSubmitClick = (event) => {
     event.preventDefault();
 
-    const formData = {
-      name: name,
-      email: email,
-      location: location,
-      password: password,
-    };
-    fetch("https://phase-3-sinatra-project.herokuapp.com/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((r) => r.json())
-      .then((newUserItem) => console.log(newUserItem));
+//     const formData = {
+//       name: name,
+//       email: email,
+//       location: location,
+//       password: password,
+//     };
+    
+    
+    
+//     fetch("https://phase-3-sinatra-project.herokuapp.com/users", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     })
+//       .then((r) => r.json())
+//       .then((newUserItem) => console.log(newUserItem));
 
-    setName("");
-    setLocation("");
-    setEmail("");
-    setPassWord("");
-    navigate("/login")
-  };
+//     setName("");
+//     setLocation("");
+//     setEmail("");
+//     setPassWord("");
+//     navigate("/login")
+   };
 
   return (
     <>
       <div className="comment-form-new">
         <h3>User Registration form :</h3>
         <form onSubmit={handleSubmitClick}>
-          <h5>Name:</h5>
+          <h5> FirstName:</h5>
           <input
             type="text"
-            name="name"
-            ref={userRef}
-            autoComplete="off"
-            onChange={(event) => setName(event.target.value)}
-            value={name}
-            placeholder="Enter your Name here"
-            onFocus={() => setUserFocus(true)}
-            onBlur={() => setUserFocus(false)}
+            name="firstname"
+            onChange={(event) => setFirstName(event.target.value)}
+            value={firstName}
+            placeholder="Enter your First Name here"
             required
           />
-          <p
-            id="uidnote"
-            className={
-              userFocus && name && !validName ? "instructions" : "offscreen"
-            }
-          >
-            4 to 24 characters.
-            <br />
-            Must begin with a letter.
-            <br />
-            Letters, numbers, underscores, hyphens allowed.
-          </p>
-
-          <h4>Location:</h4>
+         
+          <h4>Last Name:</h4>
           <input
             type="text"
-            name="location"
-            onChange={(event) => setLocation(event.target.value)}
-            value={location}
-            placeholder="Enter Your Country"
+            name="lastName"
+            onChange={(event) => setLastName(event.target.value)}
+            value={lastName}
+            placeholder="Enter Your Last Name"
             required
-            onFocus={() => setPassWordFocus(true)}
-            onBlur={() => setPassWordFocus(false)}
+           
           />
-          <h4>Email_address:</h4>
+          <h4>Year:</h4>
           <input
-            type="email"
-            name="email"
-            onChange={(event) => setEmail(event.target.value)}
-            value={email}
-            placeholder="Enter Your Email"
+            type="text"
+            name="year_study"
+            onChange={(event) => setStudy_year(event.target.value)}
+            value={year_study}
+            placeholder="Enter Your Year of Study"
             required
-            onFocus={() => setPassWordFocus(true)}
-            onBlur={() => setPassWordFocus(false)}
+            
           />
-          <h4>Password:</h4>
+          <h4>Course:</h4>
           <input
-            type="password"
-            onChange={(e) => setPassWord(e.target.value)}
-            value={password}
+            type="text"
+            onChange={(e) => setCourse(e.target.value)}
+            value={course}
             placeholder="Enter Password here"
             required
-            onFocus={() => setPassWordFocus(true)}
-            onBlur={() => setPassWordFocus(false)}
           />
-          <p
-            id="pwdnote"
-            className={
-              passWordFocus && !validPassWord ? "instructions" : "offscreen"
-            }
-          >
-            8 to 24 characters.
-            <br />
-            Must include uppercase and lowercase letters, a number and a special
-            character.
-            <br />
-            Allowed special characters:{" "}
-            <span aria-label="exclamation mark">!</span>{" "}
-            <span aria-label="at symbol">@</span>{" "}
-            <span aria-label="hashtag">#</span>{" "}
-            <span aria-label="dollar sign">$</span>{" "}
-            <span aria-label="percent">%</span>
-          </p>
-
           <button
             type="submit"
-            disabled={!validName || !validPassWord ? true : false}
+           
           >
             Register
           </button>
